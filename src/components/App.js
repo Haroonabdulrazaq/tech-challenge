2058e8112efe29b9c3712b2c1d3ff63d6ae10cd5
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-09-22 02:26:48 +0100

    Add styling to category components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import useFetch from './useFetch.js';
 import Category from './Category.js';
 import Loading from './Loading.js';
@@ -9,13 +11,17 @@ function App() {
   const { loader, data } = useFetch('https://chuckswapi20220921081349.azurewebsites.net/api/Chuck/Categories')
 
   return (
-    <div className="App">
+    <Router className="App">
       {
         loader? <Loading/>:
-      <Category categories={data.responseData} />
+      <Switch>
+        <Route exact path="/categories">
+          <Category categories={data.responseData} />
+        </Route>
+      </Switch>
       }
-    </div>
+    </Router>
   );
 }
-
+// "react-router-dom": "^5.0.0",
 export default App;
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,18 +1,26 @@
+import '../styles/category.scss'
+
 
 const Category = (props) => {
   const {categories} =  props
   console.log('Categoreis in Props', categories);
   return (
-    <>
-      <h2>All Categories</h2>
-      <div>
+    <div className='category-wrapper'>
+      <h2 className='header-2'>All Categories</h2>
+      <div className='container'>
         {
           categories.map((category)=>(
-          <div key={category.id}>{category.categoryName}</div>
+          <div className='category' key={category.id}>
+            <p className='cat-name'>{category.categoryName}</p>
+            <div className='cat-info'>
+              <p className='status'>{category.status}</p>
+              <p>{category.dateCreated}</p>
+            </div>
+          </div>
           ))
         }
       </div>
-    </>
+    </div>
   )
 }
 
